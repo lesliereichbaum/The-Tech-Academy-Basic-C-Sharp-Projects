649121acd11b1dbd120944983927fe6632c8a634
Leslie <lreichbaum@gmail.com>
2020-07-27 11:14:37 -0400

    Study Daily Report using C Sharp

---
+++ DailyReport/DailyReport/DailyReport/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+namespace DailyReport
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The Tech Academy");
+            //Console.ReadLine();
+            Console.WriteLine("Student Daily Report");
+            // Console.ReadLine();
+
+            //Questions 
+            Console.WriteLine("What is your name?");
+            string userName = Console.ReadLine();
+            Console.WriteLine("Your name is: " + userName);
+
+            Console.WriteLine("What course are you on?");
+            string courseName = Console.ReadLine();
+            Console.WriteLine("The course you are on is: " + courseName);
+
+            Console.WriteLine("What page number are you on?");
+            int page = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("The page you are on is: " + page);
+
+            Console.WriteLine("Do you need help with anything? " +
+                "Please Answer \"true\" or \"false\"");
+            bool help = Convert.ToBoolean(Console.ReadLine());
+            Console.WriteLine("You answered: " + help);
+
+            Console.WriteLine("Were there any positive experiences you'd like to share? Please give specifics.");
+            string experiences = Console.ReadLine();
+            Console.WriteLine("Thank you for sharing: " + experiences);
+
+            Console.WriteLine("Is there any other feedback you'd like to provide? Please be specific.");
+            string feedback = Console.ReadLine();
+            Console.WriteLine("I will contemplate: " + feedback);
+
+            Console.WriteLine("How many hours did you study?");
+            int hours = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("You studied:" + hours + "hours");
+
+            Console.WriteLine("Thank you for your responses. An Instructor will respond to this shortley. Have a great day!");
+
+  
+
+
+
+        }
+    }
+}
---
+++ VariablesAndDataTypes/VariablesandDataTypes/VariablesandDataTypes/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VariablesandDataTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //This is a simple program
+            //Console.WriteLine("What is your name?");
+            //string yourName = Console.ReadLine();
+            //Console.WriteLine("Your name is: " + yourName);
+            //Console.ReadLine();
+
+            //Console.WriteLine("What is your favorite number?");
+            //string favoriteNumber = Console.ReadLine();
+            ////Console.WriteLine("Your favorite number is: " + favoriteNumber);
+            ////Console.ReadLine();
+            //int favNum = Convert.ToInt32(favoriteNumber);
+            //int total = favNum + 5;
+            //Console.WriteLine("Your favorite number plus 5 is: " + total);
+            //Console.ReadLine();
+
+            bool isStudying = false;
+            byte hoursworked = 42;
+            sbyte currentTemperature = -23;
+            char questionMark = '\u2103';
+
+            decimal moneyInBank = 100.5m;
+
+            double heightInCentimeters = 211.30980989;
+
+            float secondsLeft = 2.62f;
+
+            short temperatureOnMars = -341;
+
+            string myName = "leslie";
+
+            int currentAge = 30;
+            string yearsOld = currentAge.ToString();
+
+            //Another example of Casting variable/data types:
+
+            bool isRaining = true;
+            string rainingStatus = Convert.ToString(isRaining);
+            Console.WriteLine(rainingStatus);
+            Console.ReadLine();
+
+
+            Console.WriteLine(questionMark);
+            Console.ReadLine();
+        }
+    }
+}
