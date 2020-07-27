4cd62939276841a4e7e699414e1bbf85fe3f45c3
Leslie <lreichbaum@gmail.com>
2020-07-27 18:02:34 -0400

    Car Insurance Qualification Program

---
+++ BooleanLogicProgram/BooleanLogicProgram/BooleanLogicProgram/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace BooleanLogicProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine(true && false);
+            //Console.WriteLine(true && true);
+            //Console.WriteLine(false && false);
+
+            //Console.WriteLine(true || true);
+            //Console.WriteLine(true || false);
+            //Console.WriteLine(false || false);
+
+            //Console.WriteLine(true == true);
+            //Console.WriteLine(true == false);
+            //Console.WriteLine(false == false);
+
+            //Console.WriteLine(true != true);
+            //Console.WriteLine(true != false);
+            //Console.WriteLine(false != false);
+
+            // XOr operator 
+            //Console.WriteLine(true ^ true);
+            //Console.WriteLine(true ^ false);
+            //Console.WriteLine(false ^ false);
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ CarInsuranceProgram/CarInsuranceProgram/CarInsuranceProgram/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace CarInsuranceProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your age?");
+            int userAge = Convert.ToInt32(Console.ReadLine());
+           
+
+            Console.WriteLine("Have you ever had a DUI? Answer yes or no");
+            string safeDriver = Console.ReadLine();
+
+            Console.WriteLine("How many speeding tickets do you have?");
+            int speedingTickets = Convert.ToInt32(Console.ReadLine());
+
+            if (userAge >= 16 && safeDriver != "yes" && speedingTickets <= 3)
+            {
+                Console.WriteLine("You have qualified. Congratulations");
+            }
+            else
+            {
+                Console.WriteLine("You do not meet our qualifications. Thank you for your time.");
+            }
+           
+
+        }
+    }
+}
