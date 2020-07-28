67194004a829a5f9a3ed5e86277f22d427ae1b9c
Leslie <lreichbaum@gmail.com>
2020-07-28 19:18:27 -0400

    arrays and list console app

---
+++ ArraryandListsAssignment/ArraryandListAssignment/ArraryandListAssignment/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+
+        //ARRAYS
+        //String ARRAY
+
+        //try
+        //{
+        //    Console.WriteLine("choose a number from 0 through 4. this will be your snack today.");
+        //    string[] stringarray = new string[] { "apple", "orange", "watermelon", "grapes", "banana" };
+
+
+
+        //    int fruitnum = Convert.ToInt32(Console.ReadLine());
+        //    Console.WriteLine("you will have a: " + stringarray[fruitnum]);
+        //}
+
+        //catch (Exception e)
+        //{
+        //    Console.WriteLine(e.Message);
+        //}
+
+        //INT ARRAY
+
+        //try
+        //{
+        //    Console.WriteLine("Choose a number from 0 through 4. Use this number to play the lottery.");
+        //    int[] intArray = new int[] { 32, 98, 54, 67, 19 };
+
+
+        //    int lotteryNum = Convert.ToInt32(Console.ReadLine());
+        //    Console.WriteLine("Your number is: " + intArray[lotteryNum]);
+
+        //}
+
+        //catch (Exception e)
+        //{
+        //    Console.WriteLine(e.Message);
+        //}
+
+        //LIST 
+
+        Console.WriteLine("Choose either 0, 1, 2, or 3");
+     
+
+        List<string> StarList = new List<string>();
+        StarList.Add("Twinkle");
+        StarList.Add("Twinkle");
+        StarList.Add("Little");
+        StarList.Add("Star");
+
+   
+
+      int StarSong = Convert.ToInt32(Console.ReadLine());
+      Console.WriteLine("Guess the song with just the word given: " + StarList[StarSong]); 
+      
+
+
+
+    }
+}
+
+
---
+++ ArraysandLists/ArrarysandLists/ArrarysandLists/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+    class Program
+    {
+    static void Main()
+    {
+
+        //LISTS
+
+        List<string> intList = new List<string>();
+        intList.Add("hello");
+        intList.Add("leslie");
+        intList.Remove("bye");
+
+        Console.WriteLine(intList[1]);
+        Console.ReadLine();
+
+        //ARRAY
+        //int[ ] numArray = new int[5];
+        //numArray[0] = 5;
+        //numArray[1] = 2;
+        //numArray[2] = 10;
+        //numArray[3] = 200;
+        //numArray[4] = 5000;
+
+
+
+        //int[] numArray1 = new int[] { 5, 2, 10, 200, 500 };
+
+        //int[] numArray2 = { 5, 2, 10, 201, 500, 600, 2300 };
+
+        //numArray2[5] = 650;
+
+        //Console.WriteLine(numArray2[5]);
+        //Console.ReadLine();
+
+        }
+    }
+
