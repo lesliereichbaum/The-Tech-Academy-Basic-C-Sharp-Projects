e4671e4f7581ff32f6fe0c5a6226971fa23e918b
Leslie <lreichbaum@gmail.com>
2020-07-28 15:08:31 -0400

    console app with strings

---
+++ StringAssignment/StringAssignment/StringAssignment/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Text;
+
+namespace StringAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string firstName = "Leslie ";
+            //string middleName = "Marie ";
+            //string lastName = "Reichbaum";
+            //string myname = string.Concat(firstName, middleName, lastName);
+         
+
+            //myname = myname.ToUpper();
+
+            //Console.WriteLine(myname);
+
+            StringBuilder sb = new StringBuilder();
+            sb.Append("The Ministry of Truth, which concerned itself with news, entertainment, \n education and the fine arts.");
+            sb.AppendLine("The Ministry of Peace, which is concerned itself with war.");
+            sb.AppendLine("The Ministry of Love, which maintained law and order.");
+            sb.AppendLine("-George Orwell's 1984");
+            Console.WriteLine(sb);
+
+
+
+
+        }
+    }
+}
---
+++ StringsFunctions.Program/StringsFunctions.Program/StringsFunctions.Program/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Text;
+
+namespace StringsFunctions.Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string name = "Leslie";
+            //string quote = "The man said, \"Hello\", Leslie. \n Hello on a new line. \n \t Hello on a tab.";
+            //string fileName = @"C:\Users\Leslie";
+
+            //bool trueOrFalse = name.Contains("s");
+            //trueOrFalse = name.EndsWith("s");
+
+            //int length = name.Length;
+
+            //name = name.ToLower();
+
+            //Console.WriteLine(name);
+            //Console.ReadLine();
+
+            //string name = "Leslie";
+            //name = "Brian";
+
+            StringBuilder sb = new StringBuilder();
+
+            sb.Append("My name is Leslie");
+
+            Console.WriteLine(sb);
+            Console.ReadLine();
+
+        }
+    }
+}
