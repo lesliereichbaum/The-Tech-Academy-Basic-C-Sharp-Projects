c4fd5b2311856c8b6840e161a2f14d53969da228
Leslie <lreichbaum@gmail.com>
2020-07-28 12:18:49 -0400

    Shipping Quote Console App with branching

---
+++ Branching_Program/Branching_Program/Branching_Program/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+namespace Branching_Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("what is your favorite number?");
+            int favNum = Convert.ToInt32(Console.ReadLine());
+
+            string result = favNum == 12 ? "you have an awesome fav number." : "you do not have an awesome fav number.";
+
+            Console.WriteLine(result);
+            Console.ReadLine();
+
+            //int roomTemperature = 70;
+     
+
+            //Console.WriteLine("Hi, what is your name?");
+            //string name = Console.ReadLine();
+
+            //Console.WriteLine("Hi, " + name + ", what is the temperature where you are?");
+            //int currentTemperature = Convert.ToInt32(Console.ReadLine());
+
+            //if (currentTemperature == roomTemperature)
+            //{
+            //    Console.WriteLine("it is exactly room temp.");
+            //}
+
+            //else if (currentTemperature > roomTemperature)
+            //{
+            //    Console.WriteLine("it is warmer than room temp");
+            //}
+
+            //else if (roomTemperature > currentTemperature)
+            //{
+            //    Console.WriteLine("it is colder than room temp.");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("uhhhhh...something went wrong.");
+            //}
+
+            //Console.ReadLine(); 
+
+            //int currentTemperature = 80;
+            //int roomTemperature = 70;
+
+
+            //string comparisionResult = currentTemperature == roomTemperature ? "it is room temp." : "it is not room temp,";
+
+            //Console.WriteLine(comparisionResult);
+            //Console.ReadLine();
+
+            //if (currentTemperature == roomTemperature)
+            //{
+            //    Console.WriteLine("It is exactly room temp.");
+            //}
+            //else if  (currentTemperature > roomTemperature)
+            //{
+            //    Console.WriteLine("it is warmer than room temp.");
+            //}
+            //else if (roomTemperature > currentTemperature)
+            //{
+            //    Console.WriteLine("room temp is warmer than current temp.");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("it is not exactly room temp.");
+            //}
+
+            //Console.ReadLine();
+        }
+    }
+}
---
+++ Shipping_Quotes_ConsoleApp/Shipping_Quotes_ConsoleApp/Shipping_Quotes_ConsoleApp/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace ShippingQuote_BranchingAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine("Welcome to Package Express. Please follow the instructions below.");
+
+            //Package WEIGHT
+            Console.WriteLine("Please enter the package weight.");
+            int packageWeight = Convert.ToInt32(Console.ReadLine());
+
+            if (packageWeight < 50)
+            {
+                Console.WriteLine("Please answer the following questions about l, w, and h.");
+            }
+
+
+            else if (packageWeight > 50)
+            {
+                Console.WriteLine("Package too heavy to be shipped via Package Express. Have a good day.");
+                Console.ReadLine();
+            }
+        
+            //Package Width, Height, and Length
+
+            Console.WriteLine("Please enter the package width.");
+                int packageWidth = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Please enter the package height.");
+                int packageHeight = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Please enter the package length.");
+                int packageLength = Convert.ToInt32(Console.ReadLine());
+
+
+                int dimension = packageLength * packageWidth * packageHeight;
+      
+
+            if (dimension > 125000)
+            {
+                Console.WriteLine("Package too big to be shipped via Package Express. Have a good day.");
+                Console.ReadLine();
+            }
+
+            int threeDimensions = packageLength * packageWidth * packageHeight * packageWeight / 100;
+            Console.WriteLine("Your estimated total for shipping this package is: " +" $" +  threeDimensions);
+            Console.ReadLine(); 
+
+        }
+    }
+}
