9f9d0a700665489c660b7c4535ca268e34ddecf8
Leslie <lreichbaum@gmail.com>
2020-07-30 12:15:05 -0400

    Exception Assignment

---
+++ Assignment_ErrorHandling/Assignment_ErrorHandling/Assignment_ErrorHandling/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{ 
+        static void Main(string[] args)
+        {
+        List<int> numberList = new List<int>();
+        numberList.Add(2);
+        numberList.Add(4);
+        numberList.Add(6);
+        numberList.Add(8);
+        numberList.Add(10);
+        numberList.Add(12);
+        numberList.Add(14);
+
+        foreach (int number in numberList)
+        {
+            Console.WriteLine(number);
+        }
+       
+        try
+        {
+            Console.WriteLine("Pick a number to divide each number in this list by.");
+            int numberTwo = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Dividing....");
+            
+            foreach (int number in numberList)
+            {
+                int result = number / numberTwo;
+                Console.WriteLine(result);
+            }
+            
+            Console.ReadLine();
+        }
+        catch (FormatException ex)
+        {
+            Console.WriteLine("Please type a whole number.");
+            return;
+        }
+        catch (DivideByZeroException ex)
+        {
+            Console.WriteLine("Please don't divide by zero");
+        }
+        //GENERIC ERROR MESSAGE
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+       
+        finally
+        {
+            Console.WriteLine("You must have input a non whole number. Please try again.");
+       
+        }
+        Console.ReadLine();
+    }
+    }
+
