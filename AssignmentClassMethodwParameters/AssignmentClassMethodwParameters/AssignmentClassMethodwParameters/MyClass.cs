7c5b55803b8a8f912952382096ea4344cf5bc4d9
Leslie <lreichbaum@gmail.com>
2020-08-03 10:02:25 -0400

    optional parameter - not working

---
+++ AssignmentClassMethodwParameters/AssignmentClassMethodwParameters/AssignmentClassMethodwParameters/MyClass.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MethodwParameters
+{
+    public class MyClass
+        //used =0 for optional parameter 
+    {
+        public int MultTwoInt(int firstInt, int secInt = 0)
+        {
+            int multNumb = firstInt* 10;
+
+            if (secInt != 0)
+            {
+                multNumb = multNumb * secInt;
+            }
+            return multNumb;
+
+        }
+    }
+}
---
+++ AssignmentClassMethodwParameters/AssignmentClassMethodwParameters/AssignmentClassMethodwParameters/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace MethodwParameters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input one number and it will multiply by 10");
+            int numberOne= Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Put in another number, only if you want to.");
+            int numberTwo = Convert.ToInt32(Console.ReadLine());
+
+
+            //instantiated class below 
+            MyClass firstNumber = new MyClass();
+            int myReturn = firstNumber.MultTwoInt(numberOne, numberTwo);
+            int myReturn1 = firstNumber.MultTwoInt(numberOne);
+            Console.WriteLine(myReturn);
+
+            
+           
+        }
+    }
+}
