6a1eb8874b84cd641ce5495c54a9e321d2d92f64
Leslie <lreichbaum@gmail.com>
2020-08-03 16:07:09 -0400

    step 169 class with void method, output par, overload method, and static class

---
+++ ClassOverloadMethod/ClassOverloadMethod/ClassOverloadMethod/MyClass.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassOverloadMethod
+{
+  public class MyClass
+    {
+   public  void VMethod (int divInt)
+        {
+            int intMethod = divInt/ 2;
+            Console.WriteLine(intMethod);
+        }
+    public void  VMethod (double divDouble)
+        {
+            double doubleMethod = divDouble / 1.5;
+            Console.WriteLine(doubleMethod);
+        }
+
+       static void OutPar (out int x)
+        {
+            x = 100;
+        }
+
+    }
+}
---
+++ ClassOverloadMethod/ClassOverloadMethod/ClassOverloadMethod/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ClassOverloadMethod
+{
+    class Program
+    {
+        private static readonly int divInt;
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number so that I can divide it by 2");
+            int divInt = Convert.ToInt32(Console.ReadLine());
+          
+         
+            //instantiated class
+            MyClass vClass = new MyClass();
+            vClass.VMethod(divInt);
+        }
+    }
+}
