876c1dcb2353e570f8d4104d0c2d5a8c1367318d
Leslie <lreichbaum@gmail.com>
2020-08-03 12:43:37 -0400

    class with a void method

---
+++ AssignmentVoidMethod/AssignmentVoidMethod/AssignmentVoidMethod/MyClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AssignmentVoidMethod
+{
+    public class MyClass
+    {
+        //added 'void' and removed 'int'
+        public void VMethod (int firstNumb, int secNumb)
+        {
+            int AddNumb = firstNumb + 100;
+            Console.WriteLine(secNumb);
+        }
+    }
+}
---
+++ AssignmentVoidMethod/AssignmentVoidMethod/AssignmentVoidMethod/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace AssignmentVoidMethod
+{
+    class Program
+    {
+       public static void Main(string[] args)
+        {
+            //Console.WriteLine("Type a number to add to my number");
+            int firstNumb = 100;
+
+            //Console.WriteLine("Here is my other number.");
+            int secNumb = 4;
+
+            //instantiated class 
+            MyClass vClass = new MyClass();
+            //calling method in class and passing two numbers
+            vClass.VMethod(6, 11);
+            //calling method in class and specifying parameters by name
+           vClass.VMethod(firstNumb, secNumb);
+            
+
+        }
+    }
+}
