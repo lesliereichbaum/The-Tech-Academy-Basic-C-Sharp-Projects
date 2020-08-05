023ea55c1844bf0fd512b82bf3a34807c97498cb
Leslie <lreichbaum@gmail.com>
2020-08-05 15:28:25 -0400

    generic parameters; not printing to console

--- AbstractPersonAssignment/AbstractPersonAssignment/AbstractPersonAssignment/Employee.cs
+++ AbstractPersonAssignment/AbstractPersonAssignment/AbstractPersonAssignment/Employee.cs
@@ -4,52 +4,61 @@ using System.Text;
 
 namespace AbstractPersonAssignment
 {
-    class Employee : Person, IQuittable
+    public class Employee <T>
     {
-        public int Id { get; set; } //created property called Id
+        public List <T> things { get; set; } // T instead of "strings"
 
-        public static bool operator==(Employee employee1, Employee employee2)
-        {
-            bool result;
-            if (employee1.Id == employee2.Id) // Overload operator to check if ==
-            {
-                result = true;
-                
-            }
-            else
-            {
-                result = false; 
-            }
-            return result;
-        }
-
-        public static bool operator!=(Employee employee1, Employee employee2) //Overload operator to check !=
-        {
-            bool result;
-            if (employee1.Id != employee2.Id)
-            {
-                result = true;
-           
-            }
-            else
-            {
-                result = false; 
-               
-            }
-            return result; 
-        }
-
-        public void SayName()
-        {
-            Console.WriteLine(firstName + " " + lastName);
-        }
-        public void Quit(string firstName, string lastName)
-        {
-            Console.WriteLine(firstName + lastName + "quits");
-        }
-        public int ID { get; set; } //ID property with variable int - added from Person Assignment Project
-
-     
-           
     }
+
+    //class Employee : Person, IQuittable
+    //{
+    //    public int Id { get; set; } //created property called Id
+
+    //    public static bool operator==(Employee employee1, Employee employee2)
+    //    {
+    //        bool result;
+    //        if (employee1.Id == employee2.Id) // Overload operator to check if ==
+    //        {
+    //            result = true;
+                
+    //        }
+    //        else
+    //        {
+    //            result = false; 
+    //        }
+    //        return result;
+    //    }
+
+    //    public static bool operator!=(Employee employee1, Employee employee2) //Overload operator to check !=
+    //    {
+    //        bool result;
+    //        if (employee1.Id != employee2.Id)
+    //        {
+    //            result = true;
+           
+    //        }
+    //        else
+    //        {
+    //            result = false; 
+               
+    //        }
+    //        return result; 
+    //    }
+
+    //    public void SayName()
+    //    {
+    //        Console.WriteLine(firstName + " " + lastName);
+    //    }
+    //    public void Quit(string firstName, string lastName)
+    //    {
+    //        Console.WriteLine(firstName + lastName + "quits");
+    //    }
+      
+    //    public int ID { get; set; } //ID property with variable int - added from Person Assignment Project
+           
+    //}
+
+    
+    
+    
 }
--- AbstractPersonAssignment/AbstractPersonAssignment/AbstractPersonAssignment/Program.cs
+++ AbstractPersonAssignment/AbstractPersonAssignment/AbstractPersonAssignment/Program.cs
@@ -8,25 +8,42 @@ namespace AbstractPersonAssignment
     {
         static void Main(string[] args)
         {
-            Employee myEmployee = new Employee();
+            Employee<string> thingsEmployee = new Employee<string>();
+            thingsEmployee.things = new List<string> {"p","o" };
+           
+          
+            Employee<int> numEmployee = new Employee<int>();
+            numEmployee.things = new List<int> { 9, 22 };
 
-            myEmployee.firstName = "Sample";
-            myEmployee.lastName = "Student"; //myEmployee is an object, f/l name are properties; "setting" not "getting"
+          foreach (string element in thingsEmployee.things)
+            {
+                Console.WriteLine( thingsEmployee + "{}"); //not sure how to make string print 
+            }
 
-            myEmployee.SayName();
+          foreach (int element in numEmployee.things)
+            {
+                Console.WriteLine(numEmployee); // not sure how to make int print 
+            }
 
-            IQuittable theyQuit = myEmployee;
-            theyQuit.Quit(myEmployee.firstName, myEmployee.lastName);
+            //Employee myEmployee = new Employee();
+
+            //myEmployee.firstName = "Sample";
+            //myEmployee.lastName = "Student"; //myEmployee is an object, f/l name are properties; "setting" not "getting"
+
+            //myEmployee.SayName();
+
+            //IQuittable theyQuit = myEmployee;
+            //theyQuit.Quit(myEmployee.firstName, myEmployee.lastName);
 
 
-            //Overload Operator: 
-            Employee emp1 = new Employee();  
-            emp1.Id = 1;
-            Employee emp2 = new Employee();
-            emp2.Id = 2;
+            ////Overload Operator: 
+            //Employee emp1 = new Employee();  
+            //emp1.Id = 1;
+            //Employee emp2 = new Employee();
+            //emp2.Id = 2;
 
-            Console.WriteLine(emp1 != emp2);
-            Console.ReadLine();
+            //Console.WriteLine(emp1 != emp2);
+            //Console.ReadLine();
 
   
             
