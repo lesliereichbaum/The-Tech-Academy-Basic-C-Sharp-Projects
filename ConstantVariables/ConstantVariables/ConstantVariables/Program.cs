d91f0eb4b93de5878df62935f052219299e67884
Leslie <lreichbaum@gmail.com>
2020-08-10 18:15:31 -0400

    const, var, and chaining constructors

---
+++ ConstantVariables/ConstantVariables/ConstantVariables/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+
+namespace ConstantVariables
+{
+   public class Program
+    {
+     
+        public static void Main()
+        {
+            string niceTree = "Oak";
+            Tree myTree = new Tree(niceTree);
+            int age = myTree._age;
+
+            //two local const variables 
+            const string message = "Hello, World";
+            Console.WriteLine(message);
+
+            var txt = "It's such a beautiful day.";
+            Console.WriteLine(txt);
+
+        }
+    }
+    }
+    
---
+++ ConstantVariables/ConstantVariables/ConstantVariables/Tree.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConstantVariables
+{
+    public class Tree
+    {
+        public string _type;
+        public string _color;
+        public int _age;
+        
+
+        public Tree(string type)
+            : this(type, "" , 0) {
+        }
+
+        public Tree (string type, string color)
+            : this (type, color, 0)  {
+
+        }
+
+        public Tree(string typeName, string colorName, int ageOfTree)
+        {
+            _type = typeName;
+            _color = colorName;
+            _age = ageOfTree;
+        }
+    }
+}
